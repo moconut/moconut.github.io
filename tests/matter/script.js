ec2749e0453ca67a473613eb093c90dc9daccf89
BLURTIC-LAPTOP\blurtic <asdfgh25@naver.com>
2020-06-14 19:48:14 +0900

    글씨 삽입. 클릭시 공 생성.

--- tests/matter/script.js
+++ tests/matter/script.js
@@ -1,11 +1,20 @@
-var screenWidth = 800;
-var screenHeight = 800;
+var screenWidth = window.innerWidth;
+var screenHeight =  window.innerHeight;
 var borderThickness = 20;
+var imageScale = 0.6;
+var ballRadius = 20;
+
+var colorArray = [];
+colorArray.push('rgb(47,175,74)');
+colorArray.push('rgb(255,123,255)');
 
 var Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
-    Bodies = Matter.Bodies;
+    Bodies = Matter.Bodies,
+    Mouse = Matter.Mouse,
+    MouseConstraint = Matter.MouseConstraint,
+    Events = Matter.Events;
      
 var engine = Engine.create();
  
@@ -19,33 +28,72 @@ var render = Render.create({
                     wireframes: false
                 }
              });
+
+var bodyMouse = Mouse.create(document.body);
+var options = {
+  mouse: bodyMouse
+};
+mConstraint = MouseConstraint.create(engine, options);
+World.add(engine.world, mConstraint);
+
+Events.on(mConstraint, "mousedown", function(event) {
+    console.log(mConstraint.mouse.position);
+
+    var newBall = Bodies.circle(mConstraint.mouse.position.x, mConstraint.mouse.position.y, ballRadius, 10);
+    var targetColor = Math.round(Math.random());
+    newBall.render.fillStyle = colorArray[targetColor];
+    newBall.restitution = 0.9;
+    World.add(engine.world, newBall);
+});
               
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-boxA.restitution = 0.9;
-var ballA = Bodies.circle(380, 100, 40, 10);
-ballA.restitution = 0.9;
-var ballB = Bodies.circle(460, 10, 40, 10);
-ballB.restitution = 0.9;
 var groundWall = Bodies.rectangle(screenWidth / 2, screenHeight, screenWidth, borderThickness, { isStatic: true });
 var topWall = Bodies.rectangle(screenWidth / 2, 0, screenWidth, borderThickness, { isStatic: true });
 var leftWall = Bodies.rectangle(0, screenHeight / 2, borderThickness, screenHeight, { isStatic: true });
 var rightWall = Bodies.rectangle(screenWidth, screenHeight / 2, borderThickness, screenHeight, { isStatic: true });
 
-var imageBlock = Bodies.rectangle(500, 500, 65, 94, {
-    render: {
-        sprite: {
-            texture: 'image/1.png',
-            xScale: 1,
-            yScale: 1
-        }
-    }
-}
-);
-imageBlock.restitution = 0.9;
- 
-World.add(engine.world, [boxA, ballA, ballB, groundWall, topWall, leftWall, rightWall, imageBlock]);
+World.add(engine.world, [groundWall, topWall, leftWall, rightWall]);
 
 engine.world.scale = 0.002;
  
 Engine.run(engine);
 Render.run(render);
+
+addWords();
+
+function addWords() {
+    addWord(1);
+}
+
+function addWord(index) {
+    if(index>82)
+        return;
+
+    var imagePath = "image/" + index + ".png";
+
+    var img = new Image();
+
+    img.onload = function(){
+        var height = img.height;
+        var width = img.width;
+
+        console.log(index + " imagesize : "+ width+","+height);
+        var xPos = Math.round(Math.random() * screenWidth);
+        var yPos = Math.round(Math.random() * screenHeight);
+        var imageBlock = Bodies.rectangle(xPos, yPos, width * imageScale, height * imageScale, {
+            render: {
+                sprite: {
+                    texture: imagePath,
+                    xScale: imageScale,
+                    yScale: imageScale
+                }
+            }
+        }
+        );
+        imageBlock.restitution = 0.9;
+        World.add(engine.world, imageBlock);
+
+        addWord(index+1);
+    }
+
+    img.src = imagePath = "image/" + index + ".png";
+}
