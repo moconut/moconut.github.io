cfcf668b8eb29ce50153e62d24dc36f0babdb58c
BLURTIC-LAPTOP\blurtic <asdfgh25@naver.com>
2020-06-14 20:03:37 +0900

    마우스 이벤트 matter 코드로 이동. 클릭 시에도 중력 바꾸도록 수정.

--- tests/matter/script.js
+++ tests/matter/script.js
@@ -39,13 +39,18 @@ mConstraint = MouseConstraint.create(engine, options);
 World.add(engine.world, mConstraint);
 
 Events.on(mConstraint, "mousedown", function(event) {
-    console.log(mConstraint.mouse.position);
+    var mouseX = mConstraint.mouse.position.x;
+    var mouseY = mConstraint.mouse.position.y;
 
-    var newBall = Bodies.circle(mConstraint.mouse.position.x, mConstraint.mouse.position.y, ballRadius, 10);
-    var targetColor = Math.round(Math.random());
-    newBall.render.fillStyle = colorArray[targetColor];
-    newBall.restitution = objectRestitution;
-    World.add(engine.world, newBall);
+    createBall(mouseX, mouseY);
+    changeGravity(mouseX, mouseY);
+});
+
+Events.on(mConstraint, "mousemove", function(event) {
+    var mouseX = mConstraint.mouse.position.x;
+    var mouseY = mConstraint.mouse.position.y;
+
+    changeGravity(mouseX, mouseY);
 });
               
 var groundWall = Bodies.rectangle(screenWidth / 2, screenHeight, screenWidth, borderThickness, { isStatic: true });
@@ -98,4 +103,21 @@ function addWord(index) {
     }
 
     img.src = imagePath = "image/" + index + ".png";
+}
+
+function changeGravity(xPos, yPos){
+    var xGravity = (xPos / screenWidth) * 2 - 1;
+    var yGravity = (yPos / screenHeight) * 2 - 1;
+    engine.world.gravity.x = xGravity;
+    engine.world.gravity.y = yGravity;
+}
+
+function createBall(xPos, yPos)
+{
+    var newBall = Bodies.circle(xPos, yPos, ballRadius, 10);
+    var targetColor = Math.round(Math.random());
+    newBall.render.fillStyle = colorArray[targetColor];
+    newBall.restitution = objectRestitution;
+    World.add(engine.world, newBall);
+
 }
